1780c533759d9acb7bc0ed12ce5fe97df79d1ca8
Thad Achilles <thadachi@ox-w6.msk.21-school.ru>
2022-03-06 14:10:34 +0300

    pong

--- src/pong.c
+++ src/pong.c
@@ -9,13 +9,13 @@ int drawScore(int sc1, int sc2);
 int drawBall(int ball_X, int ball_Y);
 int Winner1();
 int Winner2();
-int xBall (int ball_X, int ball_moveX);
-int yBall (int ball_Y, int ball_moveY);
+int xBall(int ball_X, int ball_moveX);
+int yBall(int ball_Y, int ball_moveY);
 int move(int ball, int ball_move);
 
 int main() {
-    int screenWidth = 81;
-    int screenHeight = 26;
+    int screenWidth = 82;
+    int screenHeight = 27;
     UpdatePlayground(screenWidth, screenHeight);
     return 0;
 }
@@ -32,7 +32,7 @@ int UpdatePlayground(int x, int y) {
     char movement;
     DrawPlayground(x, y, r1, r2, sc1, sc2, ball_X, ball_Y);
     while (r1 > 0) {
-        printf("\e[H\e{2J\e{3J");
+        printf("\e[H\e{\e{");
         scanf("%c", &movement);
         if (movement == 'A' || (movement == 'Z')) {
             r1 = rocket1(r1, movement);
@@ -40,16 +40,16 @@ int UpdatePlayground(int x, int y) {
         if (movement == 'K' || (movement == 'M')) {
             r2 = rocket2(r2, movement);
         }
-        if (sc1 != 21 || sc2 != 21) {
+        if (movement == 32 || (sc1 != 21 || sc2 != 21)) {
             ball_moveX = xBall(ball_X, ball_moveX);
             ball_X = move(ball_X, ball_moveX);
             ball_moveY = yBall(ball_Y, ball_moveY);
             ball_Y = move(ball_Y, ball_moveY);
         }
         DrawPlayground(x, y, r1, r2, sc1, sc2, ball_X, ball_Y);
-        if (ball_X == 2 && ball_Y != r1) {
+        if (ball_X == 2 && ball_Y != r1 && ball_Y != r1 + 1 && ball_Y != r1 - 1) {
             sc2++;
-        } else if (ball_X == 79 && ball_Y != r2) {
+        } else if (ball_X == 81 && ball_Y != r2 && ball_Y != r2 +1 && ball_Y != r2 -1) {
             sc1++;
         }
         if (sc1 == 21) {
@@ -64,31 +64,39 @@ int UpdatePlayground(int x, int y) {
 
 int DrawPlayground(int x, int y, int r1, int r2, int sc1, int sc2, int ball_X, int ball_Y) {
 do {
-        for (int i = 0; i <= x; i++) {
-            if ((y == 25) && (i == 40)) {
-                drawScore(sc1, sc2);
-                i = i + 4;
-            } else {
-                if ((i == ball_X) && (y == ball_Y)) {
-                    drawBall(ball_X, ball_Y) ;
-                    i = i + 1;
-                }
-                if ((i == 1) && ((y == r1) || (y == r1 + 1) || (y == r1 - 1))) {
-                    printf("]");
-                    i = i + 1;
-                }
-                if ((i == 80) && ((y == r2) || (y == r2 + 1) || (y == r2 - 1))) {
-                    printf("[");
-                    i = i + 1;
-                }
-            }
-            printf(" ");
+    for (int i = 0; i <= x; i++) {
+        if ((i == ball_X) && (y == ball_Y)) {
+            drawBall(ball_X, ball_Y);
+            i = i + 1;
         }
-        y = y - 1;
-        printf("\n");
-    } while (y > 0);
+        if ((y == 25) && (i == 40)) {
+            drawScore(sc1, sc2);
+            i = i + 3;
+        } else {
+            if ((i != 0 || i != 82) && (y == 1 || y == 26)) {
+                printf(".");
+                i = i + 1;
+            }
+            if ((i == 0 || i ==  82) && (y != 1 || y != 27)) {
+                printf(".");
+                i = i + 1;
+            }
+            if ((i == 1) && ((y == r1) || (y == r1 + 1) || (y == r1 - 1))) {
+                printf("]");
+                i = i + 1;
+            }
+            if ((i == 81) && ((y == r2) || (y == r2 + 1) || (y == r2 - 1))) {
+                printf("[.");
+                i = i + 1;
+            }
+        }
+        printf(" ");
+    }
+    y = y - 1;
     printf("\n");
-    return 0;
+    } while (y > 0);
+printf("\n");
+return 0;
 }
 
 int rocket1(int r1, char movement) {
@@ -103,7 +111,7 @@ int rocket1(int r1, char movement) {
             r1 = 3;
         }
     }
- return r1;
+return r1;
 }
 
 int rocket2(int r2, char movement) {
@@ -112,7 +120,7 @@ int rocket2(int r2, char movement) {
         if (r2 >= 24) {
             r2 = 23;
         }
-    }else if (movement == 'M') {
+    } else if (movement == 'M') {
         r2--;
         if (r2 <= 2) {
             r2 = 3;
@@ -140,21 +148,20 @@ int Winner2() {
     return 0;
 }
 
-int xBall (int ball_X, int ball_moveX) {
-        if (ball_X == 79 || ball_X == 2) {
-            ball_moveX *= (-1);
-        }
+int xBall(int ball_X, int ball_moveX) {
+    if (ball_X == 80 || ball_X == 2) {
+        ball_moveX *= (-1);
+    }
      return ball_moveX;
     }
 
-int yBall (int ball_Y, int ball_moveY) {
-    if (ball_Y == 24 || ball_Y == 1) {
+int yBall(int ball_Y, int ball_moveY) {
+    if (ball_Y == 25 || ball_Y == 2) {
         ball_moveY *= (-1);
     }
- return ball_moveY;
+return ball_moveY;
 }
 
 int move(int ball, int ball_move) {
     return ball + ball_move;
 }
-
